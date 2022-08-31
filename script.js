78e97c1fab8cfcb1269ca83cd894a2d1f948161a
M1lken01 <xmilken01x@gmail.com>
2022-08-31 15:06:30 +0200

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const hard = false;
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let currentLetter = "A";
 let score = 26 - alphabet.length;
+let miss = 0;
 
 function enter() {
     i = document.getElementById("input").value;
@@ -10,16 +12,24 @@ function enter() {
     document.getElementById("input").value = "";
     if (currentLetter.toLowerCase() == i) {
         letter(true);
+    } else {
+        if (hard) {
+            letter(false);
+        }
     }
 }
 
 function letter(correct) {
-    if (correct) {
+    if (typeof correct === "boolean") {
+        if (!correct) {
+            miss += 1;
+        }
         alphabet = alphabet.split(currentLetter).join("");
-        score = 26 - alphabet.length;
-        document.getElementById("score").innerHTML = "26/" + score
-        if (score == 26) {
+        score = (26 - miss) - alphabet.length;
+        document.getElementById("score").innerHTML = "Score: 26/" + score.toString().padStart(2, 0);
+        if (alphabet == "") {
             document.getElementById("letter").src = "letters/Computer_Error_screen.png";
+            document.getElementById("abc").innerHTML = "no letter left";
             document.getElementById("new").remove();
             document.getElementById("help").remove();
             document.getElementById("input").remove();
@@ -29,11 +39,11 @@ function letter(correct) {
             return;
         }
     }
-    currentLetter =
-        alphabet[Math.floor(Math.random() * alphabet.length)];
+    currentLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
     document.getElementById("help").innerHTML = "Help";
     document.getElementById("input").value = "";
     document.getElementById("letter").src = "letters/Error_screen_Letter_" + currentLetter + ".png ";
+    document.getElementById("abc").innerHTML = alphabet;
 }
 
 function help() {
